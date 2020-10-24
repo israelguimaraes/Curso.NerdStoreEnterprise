75be5dc411372a160c1536c44cdcffb2fa822fea
= <Israel.arauj0@live.com>
2020-10-24 16:31:00 -0300

    Obtendo dados de usuário do cookie

---
+++ src/web/NSE.WebApp.MVC/Extensions/AspNetUser.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class AspNetUser : IUser
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public AspNetUser(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public string Name => _accessor.HttpContext.User.Identity.Name;
+
+        public Guid ObterUserId()
+        {
+            return EstaAutenticado() ? Guid.Parse(_accessor.HttpContext.User.GetUserId()) : Guid.Empty;
+        }
+
+        public string ObterUserEmail()
+        {
+            return EstaAutenticado() ? _accessor.HttpContext.User.GetUserEmail() : string.Empty;
+        }
+
+        public bool EstaAutenticado()
+        {
+            return _accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public bool PossuiRole(string role)
+        {
+            return _accessor.HttpContext.User.IsInRole(role);
+        }
+
+        public IEnumerable<Claim> ObterClaims()
+        {
+            return _accessor.HttpContext.User.Claims;
+        }
+
+        public HttpContext ObterHttpContext()
+        {
+            return _accessor.HttpContext;
+        }
+
+        public string ObterUserToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentException(nameof(principal));
+
+            var claims = principal.FindFirst("sub");
+            return claims?.Value;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentException(nameof(principal));
+
+            var claim = principal.FindFirst("email");
+            return claim?.Value;
+        }
+
+        public static string GetUserToken(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentException(nameof(principal));
+
+            var claim = principal.FindFirst("JWT");
+            return claim?.Value;
+        }
+    }
+}
