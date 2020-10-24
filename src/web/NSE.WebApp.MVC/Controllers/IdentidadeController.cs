02cd757f0a95bf2cd6f0148b1842fd1d8c33cd76
= <Israel.arauj0@live.com>
2020-10-24 10:54:07 -0300

    Models, Views e Controllers de Login

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,9 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
-        
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid)
+                return View(usuarioRegistro);
+
+            return null;
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid)
+                return View(usuarioLogin);
+
+            return null;
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
